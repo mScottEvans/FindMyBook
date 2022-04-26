7c2fcf5d40b6f3dc054c45b8bd71413c337ffdbd
schillerandrew <schiller.andrew@gmail.com>
2022-04-26 15:19:27 -0700

    added navigation bar in Header.js and connected to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from './Header';
-import Footer from './Footer';
 import BestBooks from './BestBooks';
+import About from './About';
+import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -19,7 +20,9 @@ class App extends React.Component {
             <Route exact path="/">
               <BestBooks />
             </Route>
-            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+            <Route path="/about">
+              <About />
+            </Route>
           </Switch>
           <Footer />
         </Router>
--- src/Header.js
+++ src/Header.js
@@ -6,9 +6,9 @@ class Header extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
+        <Navbar.Brand>Can of Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        {/* PLACEHOLDER: render a navigation link to the about page */}
+        <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
       </Navbar>
     )
   }
