c4365e224e9b72d2f89e9fae3bb84e06429c9fd0
schillerandrew <schiller.andrew@gmail.com>
2022-04-26 20:30:09 -0700

    corrected state and modal handling

--- src/BookFormModal.js
+++ src/BookFormModal.js
@@ -10,27 +10,28 @@ class BookFormModal extends React.Component {
         show={this.props.showModal}
         onHide={this.props.hideModalHandler}
       >
-        <Modal.Header>
+        <Modal.Header closeButton>
           <Modal.Title>Add a book</Modal.Title>
         </Modal.Header>
         <Modal.Body>
         <Form onSubmit={this.props.handleBookSubmit}>
-          <Form.Group className="title" controlId="formBookTitle">
+          <Form.Group controlId="title">
             <Form.Label>Book name</Form.Label>
             <Form.Control type="text" placeholder="Enter the title of a book" />
           </Form.Group>
-          <Form.Group className="description" controlId="formBookDescription">
+          <Form.Group controlId="description">
             <Form.Label>Book description</Form.Label>
-            <Form.Control type="text" placeholder="Give us a short summary of what happens in the book -- a synopsis" />
+            <Form.Control type="text" placeholder="Give us a short summary of what happens in the book" />
           </Form.Group>
-          <Form.Group className="status" controlId="formBookStatus">
+          <Form.Group controlId="status">
             <Form.Label>Book status</Form.Label>
             <Form.Control type="text" placeholder="Is this book in print, or out of print?" />
           </Form.Group>
+          <Button type="submit">Add my book!</Button>
         </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button type="submit">Add my book!</Button>
+          
         </Modal.Footer>
       </Modal>
     )
